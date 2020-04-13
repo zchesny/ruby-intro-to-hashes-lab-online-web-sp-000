9b59c18e8656f630411637d868e7b8f66d533b46
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 18:28:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,10 +46,10 @@ def monopoly_with_fourth_tier
 					"mortgage_value"=>"$200"
 				},
 				b_and_o_railroad: {
-					"mortgage_value"=>"400"
+					"mortgage_value"=>"$400"
 				},
 				shortline: {
-					"mortgage_value"=>"800"
+					"mortgage_value"=>"$800"
 				}
 			},
 			rent_in_dollars: {
